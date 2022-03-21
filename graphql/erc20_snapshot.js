489a1dfeea66d40b1a0776082241b7ba1e5788f8
Chronos <chronos.ohm@protonmail.com>
2022-03-21 18:55:21 +0000

    Upsert, include update for column `updated_at` on insert conflict

--- graphql/erc20_snapshot.js
+++ graphql/erc20_snapshot.js
@@ -1,6 +1,6 @@
 export const INSERT_UPDATE_MANY = /* GraphQL */ `
   mutation INSERT_UPDATE_MANY($items: [erc20_snapshot_insert_input!]!) {
-    insert_erc20_snapshot(objects: $items, on_conflict: {constraint: erc20_snapshot_pkey, update_columns: gOHM_balance}) {
+    insert_erc20_snapshot(objects: $items, on_conflict: {constraint: erc20_snapshot_pkey, update_columns: [gOHM_balance, updated_at]}) {
       affected_rows
     }
   }
